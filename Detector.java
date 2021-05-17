9a46819861e95ced4bdba06c955f2b03284d4ddc
Samir Gayatri <you@example.com>
2021-05-17 22:51:04 +0530

    first commit

---
+++ Detector.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+
+import java.util.Scanner;
+
+public class Detector {
+    public static void main(String[] args) {
+        System.out.print("Enter any Number : ");
+        Scanner input = new Scanner(System.in);
+        System.out.println(input.hasNextInt());
+        
+      
+    }
+}
