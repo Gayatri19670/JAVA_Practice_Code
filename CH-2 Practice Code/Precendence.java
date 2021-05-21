e3b353ce5ac98ff7ebc8ab0fa87a9b207735a03d
Samir Gayatri <you@example.com>
2021-05-21 13:42:52 +0530

    first commit

---
+++ CH-2 Practice Code/Precendence.java
@@ -0,0 +1,11 @@
+public class Precendence {
+     public static void main(String[] args) {
+         
+        int y = 7;
+        int x = ++y * 8;
+        System.out.println(x);
+
+        float a = 7/4*9/2;
+        System.out.println(a);
+         }  
+}
--- CH-2 Practice Code/ResultingDataType.java
+++ CH-2 Practice Code/ResultingDataType.java
@@ -23,10 +23,5 @@ public class ResultingDataType {
 
 
 
-
-
-
-
-
     }
 }
