5211478d579cf882ba3abe597161b175c7a05ff8
Samir Gayatri <you@example.com>
2021-05-26 22:33:40 +0530

    update

--- prog/AWT/Demo.java
+++ prog/AWT/Demo.java
@@ -1,19 +1,22 @@
 import java.awt.*;
 
-import jdk.internal.org.objectweb.asm.Label;
-import jdk.internal.org.objectweb.asm.tree.analysis.Frame;
+
 public class Demo {
    Demo(){
     //    creating Frame
     Frame fr = new Frame("Demo Example");
-    // creating a lebal
-    Lable lb = new Label("User ID :");
+    // creating a label
+    Label lb = new Label("User ID :");
     // Adding Label to the frame
     fr.add(lb);
     // Creating Text Field
     TextField t = new TextField();
+    t.setSize(100, 10);
     // addind text field to the frame 
     fr.add(t);
+    Button b= new Button("Click me");
+    b.setBounds(60,200,160,60);
+    fr.add(b);
     // setting frame size 
     fr.setSize(500, 300);
     // setting the layout for the frame 
