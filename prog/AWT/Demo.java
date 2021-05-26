083eddf9dac9410cacaa354e4981e5352bc4f74c
Samir Gayatri <you@example.com>
2021-05-26 21:43:42 +0530

    first commit

--- prog/AWT/Demo.java
+++ prog/AWT/Demo.java
@@ -1,5 +1,23 @@
+import java.awt.*;
+
+import jdk.internal.org.objectweb.asm.Label;
+import jdk.internal.org.objectweb.asm.tree.analysis.Frame;
 public class Demo {
-    public static void main(String[] args) {
+   Demo(){
+    //    creating Frame
+    Frame fr = new Frame("Demo Example");
+    // creating a lebal
+    Lable lb = new Label("User ID :");
+    // Adding Label to the frame
+    fr.add(lb);
+    // Creating Text Field
+    TextField t = new TextField();
+    // addind text field to the frame 
+    fr.add(t);
+
+    
+
+   }
         
-    }
+   
 }
