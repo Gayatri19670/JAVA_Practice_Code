47369185c29b4e66779790fef1c7ca7b2cc340db
Samir Gayatri <you@example.com>
2021-07-07 23:30:04 +0530

    update

--- HackerRank Java Practice Code/Problem2.java
+++ HackerRank Java Practice Code/Problem2.java
@@ -1,19 +1,42 @@
+/*
+
+Task
+In this challenge, you must read  integers from stdin and then print them to stdout. Each integer must be printed on a new line. To make the problem a little easier, a portion of the code is provided for you in the editor below.
+
+Input Format
+
+There are  lines of input, and each line contains a single integer.
+
+Sample Input
+
+42
+100
+125
+Sample Output
+
+42
+100
+125
+
+*/
 
 
-import java.util.Scanner;
+import java.util.*;
 
-public class Addition {
-    
-    public static void main (String[] args) {
-        System.out.println("Taking input from user--- ");
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter first Number");
-        int num1 = sc.nextInt();
-        System.out.println("Enter Second Number");
-        int num2 = sc.nextInt();
-        int sum = num1 + num2;
-        System.out.println("The sum of these numbers = " + sum);
+public class Solution {
 
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        // Complete this line
+        // Complete this line
+        int b = scan.nextInt();
+        int c = scan.nextInt();
 
+        System.out.println(a);
+        // Complete this line 
+        
+        System.out.println(b);
+        System.out.println(c);
     }
 }
